bcd8e3d0c8ec1ba06fd8a355298eecb5c30ac1b5
Warfraid <yashambekar97@gmail.com>
2019-10-18 19:11:09 +0530

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -37,7 +37,7 @@ include $(CLEAR_VARS)
 BOARD_RECOVERY_IMAGE_PREPARE := \
     sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
     sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
-    sed -i 's/ro.product.device=.*/ro.product.device=ASUS_X00TD/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
+    sed -i 's/ro.product.device=.*/ro.product.device=RMX1921/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
 
 endif
 
